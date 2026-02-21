0f7e6ebaa3f1e2491fa6c92ee483c308da5b1aea
Md-sakib-al-hasan <mdsakibalhasanprogrammer1@gmail.com>
2026-02-21 13:51:39 +0600

    add the pop funcitons

--- linklist.py
+++ linklist.py
@@ -1,51 +1,72 @@
-
-#node class
+# node class
 class Node:
-    def __init__(self,value):
-        self.value = value 
+    def __init__(self, value):
+        self.value = value
         self.next = None
 
 
-#linkList calss
+# linkList class
 class LinkList:
-    def __init__(self,value):
+    def __init__(self, value):
         new_node = Node(value)
         self.head = new_node
         self.tail = new_node
         self.length = 1
 
 
-#append the singel noed
-    def append (self,value):
+    # append the single node
+    def append(self, value):
         new_node = Node(value)
 
-        if self.length == 0 :
+        if self.length == 0:
             self.head = new_node
             self.tail = new_node
         else:
             self.tail.next = new_node
             self.tail = new_node
-        self.length +=  1
 
-#print all linklist  
+        self.length += 1
+
+
+    # print all linklist
     def print_list(self):
         temp = self.head
 
-        while(temp is not  None):
+        while temp is not None:
             print(temp.value)
             temp = temp.next
 
+    # pop the linklist
+    def pop(self):
+        if self.length == 0:
+            return None
+
+        temp = self.head
+        pre = self.head
+
+        while temp.next:
+            pre = temp
+            temp = temp.next
+
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+
+        return temp
 
 
-
-
+# test
 my_link_list = LinkList(1)
 
 my_link_list.append(2)
 
 my_link_list.print_list()
+print("append operation is done")
 
+my_link_list.pop()
 
-
-
-
+my_link_list.print_list() 
